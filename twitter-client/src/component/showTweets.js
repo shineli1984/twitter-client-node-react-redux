3be2d6f664648eb9158d795d8cac65e82ea08c29
chantelle <maudlin.nk@gmail.com>
2019-06-06 18:12:04 +1000

    Update showTweets.js

--- twitter-client/src/component/showTweets.js
+++ twitter-client/src/component/showTweets.js
@@ -4,13 +4,21 @@ import { connect } from "react-redux";
 
 const ShowTweets=(props)=>{
 
+    const isfav=(fav,tweet)=>{
+        return fav.id_str===tweet.id_str;
+
+    }
+
+    props.tweets.map((tweet)=>{ tweet.fav=props.favList.some((fav)=>isfav(fav,tweet))})
+    console.log(  props.tweets)
+
         return (
 
-            <ul className="show-tweents">
+            <ul className="show-tweets">
                 {props.tweets.map((tweet)=>
 
                     <li key={tweet.id}><p>{tweet.text}</p>
-                        <AddLike id={tweet.id_str}/>
+                        <AddLike id={tweet.id_str} fav={tweet.fav} screen_name={tweet.screen_name}/>
                     </li>)
 
                 }
@@ -21,7 +29,8 @@ const ShowTweets=(props)=>{
 };
 
 const mapStateToProps = (state)=> ({
-    tweets: state
+    tweets: state.showTweets,
+    favList:state.favList
 
 });
 
