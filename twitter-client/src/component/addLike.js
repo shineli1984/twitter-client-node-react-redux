392ff330a5d3fcbfca0057ee482654bd09e7ea64
chantelle <maudlin.nk@gmail.com>
2019-06-06 18:11:58 +1000

    Update addLike.js

--- twitter-client/src/component/addLike.js
+++ twitter-client/src/component/addLike.js
@@ -1,48 +1,41 @@
-import React, {Component} from 'react';
-class AddLike extends Component {
+import React from 'react';
+import {fetchFavorateList} from "../actionCreators";
+import {connect} from "react-redux";
+const AddLike =(props)=>{
+  const{fav,id,screen_name}=props;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            like: false
-        };
-    }
+    const toggle=async ()=>{
 
-    toggle=()=>{
-        this.setState({like:!this.state.like},
-            ()=>{
+        let url1='';
+        fav?
+            url1 = 'http://localhost:5000/tweets/favorites/destroy/':
+            url1 = 'http://localhost:5000/tweets/favorites/create/';
 
-        let url='';
-        this.state.like?
-         url = 'http://localhost:5000/tweets/favorites/create/':
-         url = 'http://localhost:5000/tweets/favorites/destroy/';
-
-          url=url+this.props.id;
-            fetch(url, {
+          url1=url1+id;
+           await fetch(url1, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    id: this.props.id
+                    id: id
                 })
             })
 
-        })
+                 const url2 ='http://localhost:5000/tweets/favorites/list/' + screen_name;
+                 props.dispatch(fetchFavorateList(url2))
+        };
 
-    }
 
 
 
-    render() {
-        const {like}=this.state;
         return (
             <div>
-              <button  className={like?'like':'unlike'} onClick={this.toggle}>{like?'like':'add to like'} </button>
+              <button  className={fav?'like':'unlike'} onClick={toggle}>{fav?'like':'add to like'} </button>
             </div>
         );
-    }
-}
+    };
 
-export default AddLike;
+
+export default connect()(AddLike);
