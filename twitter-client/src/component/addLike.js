c11752d801c9a3b35c67f7faaf6e748d4dba2532
chantelle <maudlin.nk@gmail.com>
2019-06-06 23:40:07 +1000

    Update addLike.js

--- twitter-client/src/component/addLike.js
+++ twitter-client/src/component/addLike.js
@@ -1,41 +1,51 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {fetchFavorateList} from "../actionCreators";
 import {connect} from "react-redux";
-const AddLike =(props)=>{
-  const{fav,id,screen_name}=props;
+class AddLike extends Component {
 
-    const toggle=async ()=>{
+    constructor(props){
+        super(props);
+        this.state = {
+            like:""
+        }
+    }
+
+
+
+    toggle=async ()=>{
 
         let url1='';
-        fav?
+        this.state.like?
             url1 = 'http://localhost:5000/tweets/favorites/destroy/':
             url1 = 'http://localhost:5000/tweets/favorites/create/';
 
-          url1=url1+id;
-           await fetch(url1, {
+          url1=url1+this.props.id;
+            await fetch(url1, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    id: id
+                    id: this.props.id
                 })
             })
+        this.setState({state:!this.state.like})
 
-                 const url2 ='http://localhost:5000/tweets/favorites/list/' + screen_name;
-                 props.dispatch(fetchFavorateList(url2))
         };
 
 
+render(){
+    this.setState({like:this.props.fav});
 
+    return (
+        <div>
+            <button className={this.state.like ? 'like' : 'unlike'} onClick={this.toggle}>{this.state.like ? 'like' : 'add to like'} </button>
+        </div>
+    )
 
-        return (
-            <div>
-              <button  className={fav?'like':'unlike'} onClick={toggle}>{fav?'like':'add to like'} </button>
-            </div>
-        );
-    };
+}
+    }
 
 
 export default connect()(AddLike);
